b76cc045b3c9a77eb0614525bda68d3d1d514d93
Pruszko <konowal20@o2.pl>
2024-02-10 02:28:43 +0100

    Rework limit change for stricter override

--- src/mod_ReticleSizeLimitRemover.py
+++ src/mod_ReticleSizeLimitRemover.py
@@ -1,3 +1,32 @@
-import aih_constants
+from AvatarInputHandler import gun_marker_ctrl
 
-aih_constants.GUN_MARKER_MIN_SIZE = 1.0
+
+def overrideIn(cls, condition=lambda: True):
+
+    def _overrideMethod(func):
+        if not condition():
+            return func
+
+        funcName = func.__name__
+
+        if funcName.startswith("__"):
+            funcName = "_" + cls.__name__ + funcName
+
+        old = getattr(cls, funcName)
+
+        def wrapper(*args, **kwargs):
+            return func(old, *args, **kwargs)
+
+        setattr(cls, funcName, wrapper)
+        return wrapper
+    return _overrideMethod
+
+
+@overrideIn(gun_marker_ctrl)
+def _setupGunMarkerSizeLimits(func, dataProvider, scale=None):
+    limits = func(dataProvider, scale)
+
+    newLimits = (1.0, limits[1])
+    dataProvider.sizeConstraint = newLimits
+
+    return newLimits
